dedb79a74b3724d9b2e52fef8efc593bbe2b0cf9
Marcos <marcos21.009@gmail.com>
2024-03-26 17:11:47 -0500

    fix DeviceTransformer

--- Transformers/DeviceTransformer.php
+++ Transformers/DeviceTransformer.php
@@ -28,8 +28,32 @@ class DeviceTransformer extends JsonResource
             's_motor' => '',
             's_chassis' => ''
         ];
+        $comment = explode(", ", preg_replace(['/\s+/', '/^\s|\s$/'], [' ', ''], $this->device_data->comment));
+        if (isset($comment[0]) && !empty($comment[0])) {
+            foreach ($comment as $item) {
+                $item = explode(':', $item);
+                switch ($item[0]) {
+                    case 'imei':
+                        $data['imei'] = $item[1];
+                        break;
+                    case 'marca':
+                        $data['brand'] = $item[1];
+                        break;
+                    case 'modelo':
+                        $data['model'] = $item[1];
+                        break;
+                    case 'serialMotor':
+                        $data['s_motor'] = $item[1];
+                        break;
+                    case 'serialChassis':
+                        $data['s_chassis'] = $item[1];
+                        break;
+                    default;
+                        $data[$item[0] ?? 'default'] = $item[1] ?? 'N/A';
+                }
 
+            }
+        }
         return $data;
-
     }
 }
